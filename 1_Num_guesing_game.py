13c63a4689f899488622fec4bf5e6d3dddd76d23
abhi-2301-git <chinu23official@gmail.com>
2024-09-22 13:54:45 +0530

    Add files via upload

---
+++ 1_Num_guesing_game.py
@@ -0,0 +1,28 @@
+def check_guess(rand_num,total_guess):
+    while total_guess < 3:
+        #try block so that user enters valid number
+        try:
+            user_num=int(input("Enter a guess #:"))
+        except:
+            print("Invalid input")
+        # checking conditions
+        if user_num < rand_num:
+            print("Low")   
+        elif user_num > rand_num:
+            print("high")    
+        elif user_num == rand_num:
+            print("You win the number was "+str(rand_num))  
+            break
+        #incrementing guess & displaying guess
+        total_guess += 1
+        print("guess remaining "+str(total_guess)+"/3")
+        #if block so that on 3rd guess try again is not printed
+        if total_guess < 3:
+             print("Try again")
+    #User lost
+    if total_guess == 3:
+        print(f"Sorry you lost , the number was {rand_num}")
+        
+rand_num=42
+total_guess=0
+check_guess(rand_num,total_guess)
