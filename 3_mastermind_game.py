057261a77c837b0124828dba9573c4a20fa8b752
Abhinav Masih <chinu23official@gmail.com>
2024-10-06 18:48:09 +0530

    Update 3_mastermind_game.py

--- 3_mastermind_game.py
+++ 3_mastermind_game.py
@@ -1,29 +1,33 @@
 import random
+rand_ = str(random.randint(1000,9999))
 
-random_num=random.randint(1000,10000)
-random_num_str=str(random_num)
-correct = ['X']*4 
-tries=0
-while True: 
-    n = input("Enter a guess :")
-    tries += 1
-    if n == random_num_str and tries == 1:
-        print("Great! You guessed the number in just 1 try! You're a Mastermind!")
-        break
-    elif n == random_num_str:
-        print(f"yay you got it in {tries} try")
-        break
-    else:
-        try:
-            for i in range(0,4):
-                if n[i] == random_num_str[i]:
-                    correct[i]=n[i]
-        except IndexError as err:
-            print(err,"Enter a 4 digit guess")
-                
-    print("current progress",''.join(correct))  
-
-print("The random number was:", random_num)  
+def play():
+    global rand_
+    progress = ['X','X','X','X',]
+    print(rand_)
+    tries=0
+    while True:
+        guess = input("Enter a guess")
         
+        while len(guess) != 4 or not guess.isdigit():
+            print("Please enter exactly 4 numeric digits.")
+            guess = input("Enter a 4-digit guess: ")
 
-    
+        tries += 1
+
+        if guess == rand_ and tries == 1:
+            print("Great! You guessed the number in your 1st try You're a Mastermind!")
+            break
+
+        elif guess == rand_:
+            print(f"yay you got it in {tries} try")
+            break
+
+        for i in range(0,4):
+            if rand_[i] == guess[i]:
+                progress[i] = guess[i]
+            
+
+        print("current progress",''.join(progress))  
+
+play()
