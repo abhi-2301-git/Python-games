37f9f5aa803c96154c6de51afc59060f1e37fdff
Abhinav Masih <chinu23official@gmail.com>
2024-10-06 18:28:53 +0530

    Update 3_mastermind_game.py

--- 3_mastermind_game.py
+++ 3_mastermind_game.py
@@ -7,7 +7,7 @@ tries=0
 while True: 
     n = input("Enter a guess :")
     tries += 1
-    if n == random_num_str and tries == 0:
+    if n == random_num_str and tries == 1:
         print("Great! You guessed the number in just 1 try! You're a Mastermind!")
         break
     elif n == random_num_str:
